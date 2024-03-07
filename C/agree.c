2c1e96e4de4dfeb191cb9e7b0c746c78de56edde
Joseph Noblett <noblettjoe@gmail.com>
2024-03-06 23:44:08 -0500

    cs50

--- C/agree.c
+++ C/agree.c
@@ -4,9 +4,11 @@
 int main(void)
 {
   int on_off = 1;
+
   while (on_off)
   {
-   char res = get_char("Do you Agree to the TOS? (y/N)");
+    char res = get_char("Do you Agree to the TOS? (y/N)");
+
     if (res == 'Y' || res == 'y' )
     {
       printf("Argeed.\n");
