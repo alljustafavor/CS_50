1f70f6cd917d7647daecf7114a9cf0ac6a75eab0
Joseph Noblett <noblettjoe@gmail.com>
2024-03-08 22:37:02 -0500

    ctype.h

--- Arrays/length.c
+++ Arrays/length.c
@@ -1,22 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-  int string_length(string s);
-
   string name = get_string("Name: ");
-  length = string_length(name);
-  return length;
+  int length = strlen(name);
+  printf("%i\n", length);
 }
 
-int string_length(string s)
-{
-  int n = 0;
-  while(s[n] != '\0')
-  {
-    n++;
-  }
-  return n;
-
-}
